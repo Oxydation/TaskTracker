92848e97e13cd264fe2c2e416bb8acb4221641f2
Mathias Nigsch <mathias.nigsch@gmail.com>
2016-02-06 18:59:40 +0100

    Added new activity for timer

--- app/src/main/java/com/mathias/apps/tasktracker/TaskList.java
+++ app/src/main/java/com/mathias/apps/tasktracker/TaskList.java
@@ -1,5 +1,6 @@
 package com.mathias.apps.tasktracker;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -28,35 +31,44 @@ public class TaskList extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Open "AddTaskActivity"
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Open "AddTaskActivity"
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//
+//        // https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
+//        tasks = new ArrayList<>();
+//        listViewTasks = (ListView) findViewById(R.id.listViewTasks);
+//
+//        // Create the adapter to convert the array to views
+//        TaskAdapter adapter = new TaskAdapter(this, tasks);
+//
+//        listViewTasks.setAdapter(adapter);
+//        listViewTasks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                // Get name of selected item and show snackbar
+//                Task clickedItem = (Task) parent.getItemAtPosition(position);
+//                Snackbar.make(parent, "Clicked: " + clickedItem.getName(), Snackbar.LENGTH_LONG).setAction("Action", null).show();
+//            }
+//        });
+//
+//        //Add some items to the Arraylist
+//        adapter.add(new Task("test"));
+//
+//        //Add some items to the Arraylist
+//        adapter.add(new Task("Second"));
 
-        // https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView
-        tasks = new ArrayList<>();
-        listViewTasks = (ListView) findViewById(R.id.listViewTasks);
-
-        // Create the adapter to convert the array to views
-        TaskAdapter adapter = new TaskAdapter(this, tasks);
-
-        listViewTasks.setAdapter(adapter);
-        listViewTasks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Get name of selected item and show snackbar
-                Task clickedItem = (Task) parent.getItemAtPosition(position);
-                Snackbar.make(parent, "Clicked: " + clickedItem.getName(), Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
-
-        //Add some items to the Arraylist
-        adapter.add(new Task("test"));
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        ObjectAnimator animation = ObjectAnimator.ofInt (progressBar, "progress", 0, 100); // see this max value coming back here, we animale towards that value
+        animation.setDuration(5000); //in milliseconds
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.start();
     }
 
     @Override
