ec32341ae987167478d94bafa5670f37e95b6e38
Mathias Nigsch <mathias.nigsch@gmail.com>
2016-02-07 21:14:27 +0100

    Improved task model

--- app/src/main/java/com/mathias/apps/tasktracker/models/Task.java
+++ app/src/main/java/com/mathias/apps/tasktracker/models/Task.java
@@ -1,5 +1,7 @@
 package com.mathias.apps.tasktracker.models;
 
+import android.graphics.Color;
+
 import java.util.List;
 
 /**
@@ -8,12 +10,23 @@ import java.util.List;
 public class Task {
     private String name;
     private List<Task> subTasks;
+    private Color color;
+    private double timeEstaminated;
+    private double timeDone;
+    private boolean done;
 
-    public Task(){
+    public Task() {
     }
 
     public Task(String name) {
         this.name = name;
+    }
+
+    public Task(String name, List<Task> subTasks, Color color, double timeEstaminated) {
+        this.name = name;
+        this.subTasks = subTasks;
+        this.color = color;
+        this.timeEstaminated = timeEstaminated;
     }
 
     public String getName() {
@@ -22,5 +35,45 @@ public class Task {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public double getTimeEstaminated() {
+        return timeEstaminated;
+    }
+
+    public void setTimeEstaminated(double timeEstaminated) {
+        this.timeEstaminated = timeEstaminated;
+    }
+
+    public List<Task> getSubTasks() {
+        return subTasks;
+    }
+
+    public void setSubTasks(List<Task> subTasks) {
+        this.subTasks = subTasks;
+    }
+
+    public double getTimeDone() {
+        return timeDone;
+    }
+
+    public void setTimeDone(double timeDone) {
+        this.timeDone = timeDone;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
     }
 }
